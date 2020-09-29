0ac8d55cd9971011a5a50d598e328ec6264f8856
tomfram03 <tomfram03@gmail.com>
2020-09-29 09:17:24 -0300

    layout

--- src/calculadora.test.js
+++ src/calculadora.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import Caculadora from './caculadora';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<Caculadora />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
